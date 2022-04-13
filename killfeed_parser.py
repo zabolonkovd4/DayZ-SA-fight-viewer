ca72cabbe60864252e1206dd27f38aa32298b8db
Zabolonkov Dmitri <zab413@icloud.com>
2022-04-14 01:33:53 +0300

    Modified killfeed parser

--- killfeed_parser.py
+++ killfeed_parser.py
@@ -1,7 +1,6 @@
 import requests
 import json
 import re
-from pprint import pprint
 
 CHAT_ID = 931956812834553978
 
@@ -10,7 +9,7 @@ headers = {
 }
 
 def get_jsonn_from_discord_chat(chat_id, headers: dict):
-    url = 'https://discord.com/api/v9/channels/{channel_id}/messages'.format(channel_id=CHAT_ID)
+    url = 'https://discord.com/api/v9/channels/{channel_id}/messages'.format(channel_id=chat_id)
     message = requests.get(url, headers=headers)
     jsonn = json.loads(message.text)
     return jsonn
@@ -18,49 +17,43 @@ def get_jsonn_from_discord_chat(chat_id, headers: dict):
 def parse_jsonn_killfeed_obj(jsonn) -> list:
     kills = []
     for value in jsonn:
-        # Collect timestamp of kill
-        timestamp = value['timestamp']
-
         # Find out type of death
         death_type = re.search('(?<=KillFeed: )(.*?)(?=!)', str(value))
 
         # Find out killer and killed
         killer_name = re.search('(?<=Ник: )(.*?)(?= убил)', str(value))
-
-        #killed_name = re.findall('(?<=убил )(.*?)(?= STEAM)', str(value))
         killed_name = re.search('(?<=💀)(.*?)(?=💀)', str(value))
 
         # Finding coords
         coords = re.findall('\<(.*?)\>', str(value))
 
         if killer_name and killed_name:
-            kills.append((timestamp, death_type[0], killer_name.group(0), killed_name.group(0), coords))
-        else: 
-            kills.append((timestamp, death_type[0], None, killed_name.group(0), coords))
+            kills.append((death_type[0], killer_name.group(0), killed_name.group(0), coords))
+        else:
+            kills.append((death_type[0], None, killed_name.group(0), coords))
     return kills
 
 
-def update():
+def look_for_new_kill():
     import time
-    jsonn = get_jsonn_from_discord_chat(CHAT_ID, headers)
-    seed_kills = parse_jsonn_killfeed_obj(jsonn)
-    seed_timestamps = [] 
-    for kill in seed_kills:
-        seed_timestamps.append(kill[0]) # timestamp from tuple 
+    seed_kills = parse_jsonn_killfeed_obj(get_jsonn_from_discord_chat(CHAT_ID, headers))
 
-    print(seed_timestamps)
-
-    time.sleep(3)
-    tick_no = 0
+    found_kill = True
     while True:
         kills = parse_jsonn_killfeed_obj(get_jsonn_from_discord_chat(CHAT_ID, headers))
-        diff = set(seed_timestamps) ^ set(timestamps)
-        seed_kills = kills
-        seed_timestamps = timestamps
-        print("Tick #{no}:".format(no=tick_no), diff)
-        time.sleep(5)
-        tick_no += 1
+
+        # Kills are collected as reversed list
+        if kills[0] == seed_kills[0]:
+            if found_kill:
+                found_kill = False
+                print("Not found new kill")
+        else:
+            found_kill = True
+            print("Found new kill:", kills[0])
+            seed_kills = kills
+
+        time.sleep(3)
+
 
 if __name__ == "__main__":
-    update()
-
+    look_for_new_kill()
